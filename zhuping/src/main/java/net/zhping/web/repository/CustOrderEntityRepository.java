48132a460217f3d924d8ac1e20d79729f6a466d6
damingzms <damingzms@126.com>
2017-02-23 15:34:42 +0800

    删除

--- zhuping/src/main/java/net/zhping/web/repository/CustOrderEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package net.zhping.web.repository;
-
-import net.zhping.web.repository.custom.CustOrderEntityRepositoryCustom;
-
-public interface CustOrderEntityRepository extends CustOrderEntityRepositoryCustom, CommonRepository<CustOrderEntity, Long> {
-	
-}
